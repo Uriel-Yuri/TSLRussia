323b6700f6f648cff1d22dd97a7dc4a7cb120296
Yuri <Yuri.Gorchakov@TSLRussia.org>
2015-11-07 17:07:32 +0600

    spec

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_title = "Тестовая страница нового сайта TSLRussia.org"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
