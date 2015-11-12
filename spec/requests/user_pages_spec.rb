305217912f93d2f57bcb103427086e8d65ceaa4c
Yuri <Yuri.Gorchakov@TSLRussia.org>
2015-11-12 13:59:22 +0600

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,36 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
+
     before { visit signup_path }
 
-    it { should have_content('Регистрация') }
-    it { should have_title(full_title('Регистрация')) }
+    let(:submit) { "Создать мой аккаунт" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Имя",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Пароль",     with: "foobar"
+        fill_in "Подтверждение пароля", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 end
